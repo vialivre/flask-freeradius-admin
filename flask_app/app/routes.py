eebe11ab7bb70e049e85249aac6e20ae49c481d4
Julia Rizza <contato@juliarizza.com>
2019-08-09 11:09:39 -0300

    create base template

--- flask_app/app/routes.py
+++ flask_app/app/routes.py
@@ -1,13 +1,20 @@
 from app import app, db, user_datastore
+from flask import render_template
+from flask_security import login_required, current_user
 
 @app.before_first_request
 def setup():
-  if not user_datastore.get_user(1):
-    # create admin user
-    db.create_all()
-    user_datastore.create_user(username='admin', password='admin')
-    db.session.commit()
+    if not user_datastore.get_user(1):
+        # create admin user
+        db.create_all()
+        user_datastore.create_user(username='admin', password='admin')
+        db.session.commit()
 
 @app.route('/')
+@login_required
 def index():
-    return "Hello world!"
+    print(current_user)
+    return render_template(
+        'base.html',
+        user=current_user
+    )
