119c00534143e7f5632de60bc894954103301415
Julia Rizza <contato@juliarizza.com>
2019-08-09 11:57:09 -0300

    implement database migrations

--- flask_app/app/__init__.py
+++ flask_app/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import SQLAlchemyUserDatastore, Security
 
@@ -8,6 +9,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app.models import auth_models, radius_models
 # setup flask-security
