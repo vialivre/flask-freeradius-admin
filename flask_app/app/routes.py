433adf5a67e6783b31ca853e9635cdc80b44ba90
Julia Rizza <contato@juliarizza.com>
2019-08-09 10:37:15 -0300

    implement login page template

--- flask_app/app/routes.py
+++ flask_app/app/routes.py
@@ -2,10 +2,11 @@ from app import app, db, user_datastore
 
 @app.before_first_request
 def setup():
-  # create admin user
-  db.create_all()
-  user_datastore.create_user(username='admin', password='admin')
-  db.session.commit()
+  if not user_datastore.get_user(1):
+    # create admin user
+    db.create_all()
+    user_datastore.create_user(username='admin', password='admin')
+    db.session.commit()
 
 @app.route('/')
 def index():
