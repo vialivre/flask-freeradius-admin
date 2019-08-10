cb3bcbe0e42596c71cb844c7f854f2baf0b63a37
Julia Rizza <contato@juliarizza.com>
2019-08-10 13:53:23 -0300

    update initialization scripts

--- flask_app/boot.sh
+++ flask_app/boot.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-source venv/bin/activate
+while true; do
+    flask db upgrade
+    if [[ "$?" == "0" ]]; then
+        break
+    fi
+    echo Upgrade command failed, retrying in 5 secs...
+    sleep 5
+done
 exec gunicorn -b :5000 --access-logfile - --error-logfile - app:app
