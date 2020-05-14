ffb0e39c7751d9f0b360407500feadb5d4c1ede6
Julia Rizza <julia.rizza@grupoanga.com>
2020-05-14 10:23:48 -0300

    bind flask on ipv6

--- flask_app/boot.sh
+++ flask_app/boot.sh
@@ -7,4 +7,4 @@ while true; do
     echo Upgrade command failed, retrying in 5 secs...
     sleep 5
 done
-exec gunicorn -b :5000 --access-logfile /var/log/flask_freeradius_admin/access.log --error-logfile /var/log/flask_freeradius_admin/status.log app:app
+exec gunicorn -b 0.0.0.0:5000 -b [::0]:5000 --access-logfile /var/log/flask_freeradius_admin/access.log --error-logfile /var/log/flask_freeradius_admin/status.log app:app
